f46b082a63f75499fb89453ea54390c2a1f43343
armaBurton <armanarma@armaburton.com>
2022-02-03 14:59:51 -0800

    added space after async on app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('dry-weight: 99.5', async() => {
+test('dry-weight: 99.5', async () => {
   render(<App />);
   const linkElement = await screen.findByText(/dry-weight: 99.5/i);
   expect(linkElement).toBeInTheDocument();
